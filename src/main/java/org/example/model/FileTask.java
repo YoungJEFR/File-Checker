581212bd9bb3b27cfa3ea3f6f85cc53b24719d90
Prokofev Artemiy <artemey11@mail.ru>
2026-09-07 21:39:13 +0300

    Корректно обрабатываем null-поля в FileTask

--- src/main/java/org/example/model/FileTask.java
+++ src/main/java/org/example/model/FileTask.java
@@ -1,12 +1,20 @@
 package org.example.model;
 
 import java.nio.file.Path;
+import java.util.Objects;
 
 public record FileTask(
         Path path,
         ChangeType changeType,
         TaskSource taskSource
-) implements WorkerTask{
+) implements WorkerTask {
+
+    public FileTask {
+        Objects.requireNonNull(path, "path must not be null");
+        Objects.requireNonNull(changeType, "changeType must not be null");
+        Objects.requireNonNull(taskSource, "taskSource must not be null");
+    }
+
     public FileTask(Path path, ChangeType changeType) {
         this(path, changeType, TaskSource.NORMAL);
     }
